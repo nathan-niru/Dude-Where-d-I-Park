5dcb68b88613ac30db385a5a35441759c73a35dc
Caleb Dyck <dyck.caleb@gmail.com>
2015-11-13 15:36:46 -0800

    removed commented out code

--- tests/Controllers/controllers.tests.js
+++ tests/Controllers/controllers.tests.js
@@ -1,31 +1,3 @@
-// describe('ParkingController', function(){
-//   var scope,
-//   localStorage;
-
-//     // load the controller's modules
-//     beforeEach(module('starter.controllers'));
-//     beforeEach(module('starter.services'));
-//     beforeEach(module('ngCordovaMocks'));
-//     beforeEach(module('Constants'));
-
-//     beforeEach(inject(function(
-//       $rootScope,
-//       $controller,
-//       $localStorage,
-//       $savedParkingService,
-//       $notificationService,
-//       $timeout
-//     ) {
-//       scope = $rootScope;
-//       $controller('ParkingCtrl', {$scope: scope});
-//     }));
-
-//     // tests start here
-//     it('should have vibrate true', function(){
-//       expect(scope.vibrate).toEqual(true);
-//     });
-//   });
-
 describe('SettingsController', function(){
   var scope,
   localStorage;
