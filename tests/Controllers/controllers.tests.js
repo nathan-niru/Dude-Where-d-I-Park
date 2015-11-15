011209d2b752bf9fe936a82021c267b43b5960c2
Caleb Dyck <dyck.caleb@gmail.com>
2015-11-14 16:20:38 -0800

    Filled in controllers test file with sections for other controllers. Need to find a fix or workaround for localNotificationService

--- tests/Controllers/controllers.tests.js
+++ tests/Controllers/controllers.tests.js
@@ -1,3 +1,109 @@
+// describe('MapController', function(){
+//   var $scope,
+//       localStorage,
+//       appSettings,
+//       savedParkingService,
+//       parkingDataService,
+//       parkingCalculationService,
+//       notificationService,
+//       timeout,
+//       interval;
+
+//   beforeEach(function() {
+//     // load the controller's modules
+//     module('starter.controllers');
+//     module('starter.services');
+//     module('Constants');
+
+//     inject(function(
+//       $rootScope,
+//       $controller,
+//       $localStorage,
+//       $appSettings,
+//       $savedParkingService,
+//       $parkingDataService,
+//       $parkingCalculationService,
+//       $notificationService,
+//       $timeout,
+//       $interval
+//     ) {
+//       $scope = $rootScope;
+//       localStorage = $localStorage;
+//       appSettings = $appSettings;
+//       savedParkingService = $savedParkingService;
+//       parkingDataService = $parkingDataService;
+//       parkingCalculationService = $parkingCalculationService;
+//       notificationService = $notificationService;
+//       timeout = $timeout;
+//       interval = $interval;
+      
+//       // create controller
+//       $controller('ParkingCtrl', {
+//         $scope: $scope,
+//         $localStorage: localStorage,
+//         $appSettings: appSettings,
+//         $savedParkingService: savedParkingService,
+//         $parkingDataService: parkingDataService,
+//         $parkingCalculationService: parkingCalculationService,
+//         $notificationService: notificationService,
+//         $timeout: timeout,
+//         $interval: interval
+//       });
+//     });
+//   });
+
+//   // tests start here
+//   it('should have vibrate true', function(){
+//     expect(true).toEqual(true);
+//   });
+// });
+
+
+// describe('ParkingController', function(){
+//   var $scope,
+//       localStorage,
+//       savedParkingService,
+//       notificationService,
+//       timeout;
+
+//   beforeEach(function() {
+//     // load the controller's modules
+//     module('starter.controllers');
+//     module('starter.services');
+//     module('Constants');
+
+//     inject(function(
+//       $rootScope,
+//       $controller,
+//       $localStorage,
+//       $savedParkingService,
+//       $notificationService,
+//       $timeout
+//     ) {
+//       $scope = $rootScope;
+//       localStorage = $localStorage;
+//       savedParkingService = $savedParkingService;
+//       notificationService = $notificationService;
+//       timeout = $timeout;
+      
+//       // create controller
+//       $controller('ParkingCtrl', {
+//         $scope: $scope,
+//         $localStorage: localStorage,
+//         $savedParkingService: savedParkingService,
+//         $notificationService: notificationService,
+//         $timeout: timeout
+//       });
+//     });
+//   });
+
+//   // tests start here
+//   it('should have vibrate true', function(){
+//     expect(true).toEqual(true);
+//   });
+// });
+
+
 describe('SettingsController', function(){
   var $scope,
       localStorage,
